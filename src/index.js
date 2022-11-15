89b3a53c66918f71cd517112a8b461db23dc0ddd
SinghVivek007 <105438190+SinghVivek007@users.noreply.github.com>
2022-11-15 06:44:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,167 +1,38 @@
-/*
-*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
-*
-*  Use of this source code is governed by a BSD-style license
-*  that can be found in the LICENSE file in the root of the source
-*  tree.
-*/
+let btn = document.querySelector(".record-btn")
 
-// This code is adapted from
-// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
+btn.addEventListener("click", async function () {
+  let stream = await navigator.mediaDevices.getDisplayMedia({
+    video: true
+  })
 
-'use strict';
+  //needed for better browser support
+  const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9") 
+             ? "video/webm; codecs=vp9" 
+             : "video/webm"
+    let mediaRecorder = new MediaRecorder(stream, {
+        mimeType: mime
+    })
 
-/* globals MediaRecorder */
+    let chunks = []
+    mediaRecorder.addEventListener('dataavailable', function(e) {
+        chunks.push(e.data)
+    })
 
-var mediaSource = new MediaSource();
-mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
-var mediaRecorder;
-var recordedBlobs;
-var sourceBuffer;
+    mediaRecorder.addEventListener('stop', function(){
+      let blob = new Blob(chunks, {
+          type: chunks[0].type
+      })
+      let url = URL.createObjectURL(blob)
 
-var gumVideo = document.querySelector('video#js-video-recorder');
-var recordedVideo = document.querySelector('video#js-video-player');
+      let video = document.querySelector("video")
+      video.src = url
 
-var recordButton = document.querySelector('button#js-button-record');
-var playButton = document.querySelector('button#js-button-play');
-var downloadButton = document.querySelector('button#js-button-download');
-recordButton.onclick = toggleRecording;
-playButton.onclick = play;
-downloadButton.onclick = download;
+      let a = document.createElement('a')
+      a.href = url
+      a.download = 'video.webm'
+      a.click()
+  })
 
-// window.isSecureContext could be used for Chrome
-var isSecureOrigin = location.protocol === 'https:' ||
-location.hostname === 'localhost';
-if (!isSecureOrigin) {
-  alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
-    '\n\nChanging protocol to HTTPS');
-  location.protocol = 'HTTPS';
-}
-
-var constraints = {
-  audio: true,
-  video: true
-};
-
-function handleSuccess(stream) {
-  recordButton.disabled = false;
-  console.log('getUserMedia() got stream: ', stream);
-  window.stream = stream;
-  gumVideo.srcObject = stream;
-}
-
-function handleError(error) {
-  console.log('navigator.getUserMedia error: ', error);
-  if (error.name === "NotAllowedError") {
-    alert(`You need to allow access to your camera and microphone for this page.`); 
-  }
-}
-
-navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
-
-function handleSourceOpen(event) {
-  console.log('MediaSource opened');
-  sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
-  console.log('Source buffer: ', sourceBuffer);
-}
-
-recordedVideo.addEventListener('error', function(ev) {
-  console.error('MediaRecording.recordedMedia.error()');
-  alert('Your browser can not play\n\n' + recordedVideo.src
-    + '\n\n media clip. event: ' + JSON.stringify(ev));
-}, true);
-
-function handleDataAvailable(event) {
-  if (event.data && event.data.size > 0) {
-    recordedBlobs.push(event.data);
-  }
-}
-
-function handleStop(event) {
-  console.log('Recorder stopped: ', event);
-}
-
-function toggleRecording() {
-  if (recordButton.textContent === 'Start Recording') {
-    startRecording();
-  } else {
-    stopRecording();
-    recordButton.textContent = 'Start Recording';
-    playButton.disabled = false;
-    downloadButton.disabled = false;
-  }
-}
-
-function startRecording() {
-  recordedBlobs = [];
-  var options = {mimeType: 'video/webm;codecs=vp9'};
-  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-    console.log(options.mimeType + ' is not Supported');
-    options = {mimeType: 'video/webm;codecs=vp8'};
-    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-      console.log(options.mimeType + ' is not Supported');
-      options = {mimeType: 'video/webm'};
-      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-        console.log(options.mimeType + ' is not Supported');
-        options = {mimeType: ''};
-      }
-    }
-  }
-  try {
-    mediaRecorder = new MediaRecorder(window.stream, options);
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder: ' + e);
-    alert('Exception while creating MediaRecorder: '
-      + e + '. mimeType: ' + options.mimeType);
-    return;
-  }
-  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-  recordButton.textContent = 'Stop Recording';
-  playButton.disabled = true;
-  downloadButton.disabled = true;
-  mediaRecorder.onstop = handleStop;
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start(10); // collect 10ms of data
-  console.log('MediaRecorder started', mediaRecorder);
-}
-
-function stopRecording() {
-  mediaRecorder.stop();
-  console.log('Recorded Blobs: ', recordedBlobs);
-  recordedVideo.controls = true;
-}
-
-function play() {
-  var superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-  recordedVideo.src = window.URL.createObjectURL(superBuffer);
-  // workaround for non-seekable video taken from
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=642012#c23
-  recordedVideo.addEventListener('loadedmetadata', function() {
-    if (recordedVideo.duration === Infinity) {
-      recordedVideo.currentTime = 1e101;
-      recordedVideo.ontimeupdate = function() {
-        recordedVideo.currentTime = 0;
-        recordedVideo.ontimeupdate = function() {
-          delete recordedVideo.ontimeupdate;
-          recordedVideo.play();
-        };
-      };
-    }
-  });
-}
-
-function download() {
-  var blob = new Blob(recordedBlobs, {type: 'video/webm'});
-  var url = window.URL.createObjectURL(blob);
-  var a = document.createElement('a');
-  a.style.display = 'none';
-  a.href = url;
-  a.download = 'test.webm';
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(function() {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 100);
-}
+    //we have to start the recorder manually
+    mediaRecorder.start()
+})
